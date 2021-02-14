aea044800f461583d1aa86e0b8279e8630b6ee1e
Jot Powers <jot.powers@gmail.com>
2021-02-14 11:15:38 -0700

    Deploy website - based on c180193d028db0695769a84614c9392f5858beda

---
+++ 9de14828.007f2c55.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[53],{125:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/ds2/blog/welcome"},{"title":"Hello","permalink":"/ds2/blog/hello-world"},{"title":"Hola","permalink":"/ds2/blog/hola"}]}')}}]);
