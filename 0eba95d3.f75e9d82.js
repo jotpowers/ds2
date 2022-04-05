3508b8b919a841ab3d95a9c5584f43a3d89d2edc
Jot Powers <jot.powers@gmail.com>
2022-04-05 11:24:29 -0700

    Deploy website - based on 211425cdbbd872a9f90327734de958ca5c0faa02

---
+++ 0eba95d3.f75e9d82.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{75:function(a){a.exports=JSON.parse('{"allTagsPath":"/ds2/blog/tags","slug":"hola","name":"hola","count":1,"permalink":"/ds2/blog/tags/hola"}')}}]);
---
+++ be78233f.c717a640.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[74],{142:function(s){s.exports=JSON.parse('{"allTagsPath":"/ds2/blog/tags","slug":"docusaurus","name":"docusaurus","count":3,"permalink":"/ds2/blog/tags/docusaurus"}')}}]);
