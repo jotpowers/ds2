20e519193ec4a0747d12233f2d4c4ea7fa74033d
Jot Powers <jot.powers@gmail.com>
2022-10-22 13:39:05 -0700

    Deploy website - based on 211425cdbbd872a9f90327734de958ca5c0faa02

---
+++ 9de14828.428969f0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[64],{133:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/ds2/blog/welcome"},{"title":"Hello","permalink":"/ds2/blog/hello-world"},{"title":"Hola","permalink":"/ds2/blog/hola"}]}')}}]);
